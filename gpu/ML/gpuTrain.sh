fe30d587f606b7237a3b8097a3da4583de749765
aaronw5 <139774687+aaronw5@users.noreply.github.com>
2025-04-10 04:42:26 -0500

    Update gpuTrain.sh

--- gpu/ML/gpuTrain.sh
+++ gpu/ML/gpuTrain.sh
@@ -7,8 +7,8 @@
 #PBS -l filesystems=home:grand:eagle
 
 # Log output and error files
-#PBS -o logs/matMul.out
-#PBS -e logs/matMul.err
+#PBS -o logs/mnist.out
+#PBS -e logs/mnist.err
 
 cd ${PBS_O_WORKDIR}
 module use /soft/modulefiles; module load conda; conda activate base
